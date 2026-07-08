eaa362a4f668f8a0c878f0731a7a492ce5fe6e67
Balazs Krizsan <krizsan.balazs@gmail.com>
2026-07-08 22:42:19 +0200

    remove old argo ingress

--- src/scripts/local/argocd-add-extensions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl delete -f ./../../addon-ingress/local/argocd-ingress.yaml \
-  -n argocd \
-  --ignore-not-found
-kubectl apply -f ./../../addon-ingress/local/argocd-ingress.yaml \
-  -n argocd
