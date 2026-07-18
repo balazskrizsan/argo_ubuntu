3169c7ed9264c553ec9adc3ccb405f2ab6250136
Balazs Krizsan <krizsan.balazs@gmail.com>
2026-07-18 18:28:15 +0200

    ubuntu/set-or-update-certs.sh added

---
+++ src/scripts/ubuntu/set-or-update-certs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p /argo_ubuntu_certs_secrets
+
+curl -o "/argo_ubuntu_certs_secrets/certificate.crt" \
+  "https://smart-scrum-poker-secrets.s3.us-east-1.amazonaws.com/cert--smart-scrump-poker.stack/v11/certificate.crt"
+
+curl -o "/argo_ubuntu_certs_secrets/private.key" \
+  "https://smart-scrum-poker-secrets.s3.us-east-1.amazonaws.com/cert--smart-scrump-poker.stack/v11/private-decrypted.key"
+
+curl -o "/argo_ubuntu_certs_secrets/private-orig.key" \
+  "https://smart-scrum-poker-secrets.s3.us-east-1.amazonaws.com/cert--smart-scrump-poker.stack/v11/private.key"
