9b23add2b8b42e576b6ff589b7d69fa065da2268
Balazs Krizsan <krizsan.balazs@gmail.com>
2026-07-18 18:56:15 +0200

    ubuntu/set-or-update-certs.sh added

--- src/scripts/ubuntu/set-or-update-certs.sh
+++ src/scripts/ubuntu/set-or-update-certs.sh
@@ -10,3 +10,5 @@ curl -o "/argo_ubuntu_certs_secrets/private.key" \
 
 curl -o "/argo_ubuntu_certs_secrets/private-orig.key" \
   "https://smart-scrum-poker-secrets.s3.us-east-1.amazonaws.com/cert--smart-scrump-poker.stack/v11/private.key"
+
+sudo systemctl restart nginx
