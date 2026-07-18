785acda5b02b559433bddcc8f3d9f0f4f0d91a4c
Balazs Krizsan <krizsan.balazs@gmail.com>
2026-07-18 23:46:32 +0200

    Prod config updates

--- src/scripts/ubuntu/argocd-remove-finalizers.sh
+++ src/scripts/ubuntu/argocd-remove-finalizers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kubectl patch job secret-manager-job -n ssp-local --type=merge -p '{"metadata":{"finalizers":[]}}'
-kubectl patch job cert-manager-job -n ssp-local --type=merge -p '{"metadata":{"finalizers":[]}}'
+kubectl patch job secret-manager-job -n ssp-prod --type=merge -p '{"metadata":{"finalizers":[]}}'
+kubectl patch job cert-manager-job -n ssp-prod --type=merge -p '{"metadata":{"finalizers":[]}}'
