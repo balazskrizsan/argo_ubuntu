b02f4cf47fcd2b758e790be8465bc9044fa7a255
Balazs Krizsan <krizsan.balazs@gmail.com>
2026-07-20 00:16:59 +0200

    Prod config updates

--- src/scripts/ubuntu/_delete.sh
+++ src/scripts/ubuntu/_delete.sh
@@ -2,7 +2,7 @@
 
 #./stop-argo-apps.sh
 ./stop-argo-app.sh elk
-#./stop-argo-app.sh ssp
-#./stop-argo-app.sh cluster
+./stop-argo-app.sh ssp
+./stop-argo-app.sh cluster
 ./argocd-remove-finalizers.sh
 ./delete-namespaces.sh
