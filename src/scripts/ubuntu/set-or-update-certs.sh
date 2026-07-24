614524d44bfca4ddb3971823938bcf895c2359de
Balazs Krizsan <krizsan.balazs@gmail.com>
2026-07-24 13:36:07 +0200

    Prod config updates

--- src/scripts/ubuntu/set-or-update-certs.sh
+++ src/scripts/ubuntu/set-or-update-certs.sh
@@ -3,12 +3,12 @@
 mkdir -p /argo_ubuntu_certs_secrets
 
 curl -o "/argo_ubuntu_certs_secrets/certificate.crt" \
-  "https://smart-scrum-poker-secrets.s3.us-east-1.amazonaws.com/cert--smart-scrump-poker.stack/v11/certificate.crt"
+  "https://smart-scrum-poker-secrets.s3.us-east-1.amazonaws.com/cert--smart-scrump-poker.stack/v12/certificate.crt"
 
 curl -o "/argo_ubuntu_certs_secrets/private.key" \
-  "https://smart-scrum-poker-secrets.s3.us-east-1.amazonaws.com/cert--smart-scrump-poker.stack/v11/private-decrypted.key"
+  "https://smart-scrum-poker-secrets.s3.us-east-1.amazonaws.com/cert--smart-scrump-poker.stack/v12/private-decrypted.key"
 
 curl -o "/argo_ubuntu_certs_secrets/private-orig.key" \
-  "https://smart-scrum-poker-secrets.s3.us-east-1.amazonaws.com/cert--smart-scrump-poker.stack/v11/private.key"
+  "https://smart-scrum-poker-secrets.s3.us-east-1.amazonaws.com/cert--smart-scrump-poker.stack/v12/private.key"
 
 sudo systemctl restart nginx
